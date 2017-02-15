65937ce54099078011adebea9a7e297c7dc02f07
Max Bendick <maxbendick@gmail.com>
2017-02-15 13:48:01 -0800

    Add widgets and style

--- src/index.js
+++ src/index.js
@@ -32,6 +32,41 @@ const CardExampleWithAvatar = () => (
   </Card>
 );
 
+const wrapCard = (title, subtitle, cardInnerJSX) => (
+  <Card style={{margin: "10px", width: "100%"}}>
+    <CardTitle title={title} subtitle={subtitle} />
+    <CardText>
+      {cardInnerJSX}
+    </CardText>
+    <CardActions>
+      <FlatButton label="Action1" />
+      <FlatButton label="Action2" />
+    </CardActions>
+  </Card>
+);
+
+const cards = [
+	wrapCard("Workplace", "Where the person works", (
+		<div>
+			<ul>
+				<li>
+					Google 2017 - Current
+					<br/>
+					Lead Google to make a lot of money. Sold the company to Alta Vista.
+				</li>
+				<li>
+					Cal Poly Orfalea College of Business 2015 - 2017
+				</li>
+				<li>
+					Adobe 2016 - 2016
+				</li>
+			</ul>
+		</div>
+	)),
+	wrapCard("Hobbies", "The person's hobbies", <div>hello card</div>),
+	wrapCard("Personality", "What's... the deal... with INTJs?!", <div>hello card</div>),
+	wrapCard("Connections", "Who the person knows", <div>hello card</div>)
+]
 
 const App = () => (
   <MuiThemeProvider>
@@ -39,26 +74,47 @@ const App = () => (
       <AppBar
 		title="Worm Files"
 	    />
-	  <h1 style={{padding: "1%", display: "inline-block"}}>David Cornella</h1>
-	  <h2 style={{display: "inline-block", color: "#444", marginLeft: "10px"}}>CEO at Google</h2>
-	  
 	  <Flexbox flexDirection="row">
-	    <Flexbox>
-	      <CardExampleWithAvatar />
-	    </Flexbox>
-	    <Flexbox>
-	      <CardExampleWithAvatar />
-	    </Flexbox>
-	  </Flexbox>
+		  <Flexbox flexGrow={1}>
+		  </Flexbox>
+		  <Flexbox>
 
-	  <Flexbox flexDirection="row">
-	    <Flexbox flexGrow={4}>
-	      <CardExampleWithAvatar />
-	    </Flexbox>
-	    <Flexbox flexGrow={2}>
-	      <CardExampleWithAvatar />
-	    </Flexbox>
-	  </Flexbox>
+
+		<Paper style={{marginTop: "20px", padding: "30px", paddingTop: "0px"}}zDepth={2}>
+	<div style={{minWidth: "900px"}}>
+
+		<h1 style={{padding: "1%", display: "inline-block"}}>David Cornella</h1>
+		<h2 style={{display: "inline-block", color: "#444", marginLeft: "10px"}}>CEO at Google</h2>
+		
+		<Flexbox flexDirection="row">
+			<Flexbox flexGrow={1}>
+			{cards[0]}
+			</Flexbox>
+			<Flexbox flexGrow={2}>
+			{cards[1]}
+			</Flexbox>
+		</Flexbox>
+
+		<Flexbox flexDirection="row">
+			<Flexbox flexGrow={1}>
+			{cards[2]}
+			</Flexbox>
+			<Flexbox flexGrow={1}>
+			{cards[3]}
+			</Flexbox>
+		</Flexbox>
+
+	  </div>
+	  </Paper>
+
+
+
+		  </Flexbox>
+		  <Flexbox flexGrow={1}>
+		  </Flexbox>
+	   </Flexbox>
+
+	  
 
 	</div>
 
