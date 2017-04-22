aa3db2f2255832f5ee559d7d71ac48a8258dcba4
Max Bendick <maxbendick@gmail.com>
2017-04-22 12:43:10 -0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -6,161 +6,47 @@ import './styles/styles.scss';
 
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import {Card, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Flexbox from 'flexbox-react';
 
 import { twitterCard, AsyncTwitter } from './components/infocards/twitter-card';
-// import { Timeline } from 'react-twitter-widgets';
-//import 'whatwg-fetch';
-
-// import { profile } from './components/profile';
-
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-/*
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			jsonResponse: null
-		};
-	}
-	
-	componentDidMount() {
-		fetch('./user.json')
-		    .then( response => response.json())
-			.then( jsonResponse => this.setState({ jsonResponse: jsonResponse }) )
-			.catch( ex => console.log('json parsing failed', ex) )
-	}
 
-	render() {
-		return (
-			<MuiThemeProvider>
-				<div style={{marginBottom: "20px"}}>
-					<AppBar title="Worm Files"/>
-						{this.state.jsonResponse ? profile(this.state.jsonResponse) : ""	}
-				</div>
-			</MuiThemeProvider>
-		);
-	}
-}
-*/
-
-const wrapCard = (title, subtitle, cardInnerJSX) => (
+const wrapCard = (title, markdown) =>
   <Card style={{margin: "10px", width: "100%"}}>
-    <CardTitle title={title} subtitle={subtitle} />
-    <CardText>
-		{cardInnerJSX}
-    </CardText>
-    {/*<CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>*/}
-  </Card>
-);
-
-const wrapCardRM = (title, subtitle, markdown) => (
-  <Card style={{margin: "10px", width: "100%"}}>
-    <CardTitle title={title} subtitle={subtitle} /> 
+    <CardTitle title={title}  /> 
 	<ReactMarkdown source={markdown} />
   </Card>
-);
 
-const cards = [
-	wrapCard("Workplace", "Where the person works", (
-		<div>
-			<ul>
-				<li>
-					Google 2017 - Current
-					<br/>
-					Lead Google to make a lot of money. Sold the company to Alta Vista.
-				</li>
-				<li>
-					Cal Poly Orfalea College of Business 2015 - 2017
-				</li>
-				<li>
-					Adobe 2016 - 2016
-				</li>
-			</ul>
-		</div>
-	)),
-	//wrapCard("Hobbies", "The person's hobbies", <div>hello card</div>),
-	//wrapCard("Personality", "What's... the deal... with INTJs?!", <div>hello card</div>),
-	//wrapCard("Connections", "Who the person knows", <div>hello card</div>),
-	twitterCard("potus") //<AsyncTwitter></AsyncTwitter>
-]
 
-/*const App = () => (
-<MuiThemeProvider>
-	<div style={{marginBottom: "20px"}}>
-		<AppBar title="Worm Files"/>
-		<Flexbox flexDirection="row">
-			<Flexbox flexGrow={1} />  {/* to keep content centered *}
+const createRows = cards => cards.map(
+    (c,i) =>
+        <Flexbox flexGrow={i+1} key={i}>
+            {wrapCard(c.title, c.content)}
+        </Flexbox>)
 
-			<Flexbox>
-				<Paper style={{marginTop: "20px", padding: "30px", paddingTop: "0px"}}zDepth={2}>
-					<div style={{minWidth: "900px"}}>					 	
-					{/* Profile information *}
-						<div id='profile-header' style={{paddingTop: "20px"}}>
-							<img width='100px' src='https://media.licdn.com/media/AAEAAQAAAAAAAAmEAAAAJDNjMDVmNzdkLTlmYWYtNGU1OC1hNjUwLWQ2MmQ4OWY1ZTY2OQ.jpg' />
-							<h1 style={{padding: "1%", display: "inline-block"}}>David Cornella</h1>
-							<h2 style={{display: "inline-block", color: "#444", marginLeft: "10px"}}>CEO at Google</h2>
-						</div>
+const renderLoading = () => <div>Loading</div>
 
-						{/* First row of cards *}
-						<Flexbox flexDirection="row">
-							<Flexbox flexGrow={1}>
-								{cards[0]}
-							</Flexbox>
-							<Flexbox flexGrow={2}>
-								{cards[1]}
-							</Flexbox>
-						</Flexbox>
-
-						{/* Second row of cards *}
-						<Flexbox flexDirection="row">
-							<Flexbox flexGrow={1}>
-								{cards[2]}
-							</Flexbox>
-							<Flexbox flexGrow={1}>
-								{cards[3]}
-							</Flexbox>
-							<Flexbox>
-								{cards[4]}
-							</Flexbox>
-						</Flexbox>
-						
-					</div>
-				</Paper>
-			</Flexbox>
-
-			<Flexbox flexGrow={1} />  {/* to keep content centered *}
-		</Flexbox>
-	</div>
-</MuiThemeProvider>
-);*/
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {profile: null}
-		this.list;
     }
 
-	
-    /*componentDidMount() {
+    componentDidMount() {
 
         const compile = extendedMarkdown => { console.error("Not implemented - talk to Max"); return {
             right: {
-                name: "David",
-                currentPosition: "CEO",
-                currentCompany: "Google",
+                name: "Chris del Chris",
+                currentPosition: "Janitor",
+                currentCompany: "Macy's",
                 pictureUrl: "https://media.licdn.com/media/AAEAAQAAAAAAAAmEAAAAJDNjMDVmNzdkLTlmYWYtNGU1OC1hNjUwLWQ2MmQ4OWY1ZTY2OQ.jpg",
                 twitter: "potus", 
                 cards: [{
@@ -192,81 +78,59 @@ class App extends React.Component {
 		
 
         Promise.resolve({asdf:"asdf"})
-        //fetch("our url -- change this")
-        //.then(r => r.json())
-        .then(json => { console.error('Talk to Logan to get the url'); return json; })
+        .then(json => { console.error('Talk to Max to get the url'); return json; })
         .then(compile)
         .then(profile => {
             this.setState({profile: profile})
         })
-    } */
-
-    renderLoading() {
-        return <div>Loading</div>
     }
 
-	createRows(cards) {
-		var output = [];
-        for(var i = 0; i < cards.length; i++) {
-		output.push(<Flexbox flexGrow={i+1}>
-				{wrapCardRM(this.state.profile.right.cards[i].title, "", this.state.profile.right.cards[i].content)}
-			</Flexbox>);
-		}
-        return output;
-	}
-
     renderLoadedProfile() {
-		var React = require('react');
-		var ReactMarkdown = require('react-markdown');
+
+        if (this.state.profile.left) {
+            console.error("Profile failed to load. Error was:", this.state.profile.left);
+            return <div>Profile failed to load</div>
+        }
+
+        const profile = this.state.profile.right;
+
+        const positionTitleJoiner = (profile.currentPosition && profile.currentPosition.length > 0
+                                    && profile.currentCompany && profile.currentCompany.length > 0)
+                                        ? "at"
+                                        : "";
 	
-        return (<Paper style={{marginTop: "20px", padding: "30px", paddingTop: "0px"}}zDepth={2}>
-            <div style={{minWidth: "900px"}}>					 	
-            {/* Profile information */}
-                <div id='profile-header' style={{paddingTop: "20px"}}>
-                    <img width='100px' src={this.state.profile.right.pictureUrl} />
-                    <h1 style={{padding: "1%", display: "inline-block"}}>{this.state.profile.right.name}</h1>
-                    <h2 style={{display: "inline-block", color: "#444", marginLeft: "10px"}}>
-						{this.state.profile.right.currentPosition} at {this.state.profile.right.currentCompany}</h2>
+        return (
+            <Paper style={{marginTop: "20px", padding: "30px", paddingTop: "0px"}}zDepth={2}>
+                <div style={{minWidth: "900px"}}>					 	
+                {/* Profile information */}
+                    <div id='profile-header' style={{paddingTop: "20px"}}>
+                        <img width='100px' src={profile.pictureUrl} />
+                        <h1 style={{padding: "1%", display: "inline-block"}}>{profile.name}</h1>
+                        
+                        <h2 style={{display: "inline-block", color: "#444", marginLeft: "10px"}}>
+                            {profile.currentPosition} {positionTitleJoiner} {profile.currentCompany}</h2>
+                    </div>
+
+                    {/* First row of cards */}
+                    <Flexbox flexDirection="column">
+                        {createRows(profile.cards)}
+                    </Flexbox>
                 </div>
-
-                {/* First row of cards */}
-                <Flexbox flexDirection="column">
-				
-					{this.createRows(this.state.profile.right.cards)}
-					
-                </Flexbox>
-
-                {/*Second row of cards*/}
-				{/*
-                <Flexbox flexDirection="row">
-                    <Flexbox flexGrow={1}>
-                        {cards[2]}
-                    </Flexbox>
-                    <Flexbox flexGrow={1}>
-                        {cards[3]}
-                    </Flexbox>
-                    <Flexbox>
-                        {cards[4]}
-                    </Flexbox>
-                </Flexbox>
-				*/}
-                
-            </div>
-        </Paper>)
+            </Paper>)
     }
 
     render() {
         return (
             <MuiThemeProvider>
 				<div style={{marginBottom: "20px"}}>
-					<AppBar title="Worm Files"/>
+					<AppBar title="Vertible" iconElementLeft={<div></div>}/>
 					<Flexbox flexDirection="row">
 						<Flexbox flexGrow={1} />  {/* to keep content centered */}
 					<Flexbox>
 					{
 						this.state.profile && this.state.profile.right
 							? this.renderLoadedProfile()
-							: this.renderLoading()
+							: renderLoading()
 					}
 					</Flexbox>
 						<Flexbox flexGrow={1} />  {/* to keep content centered */}
