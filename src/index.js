4f28ddee355e03c03d05ef6dd794583868dd0a46
Max Bendick <maxbendick@gmail.com>
2017-04-23 16:30:58 -0700

    Ready for prod. Getting info from server.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactMarkdown from 'react-markdown';
 import {render} from 'react-dom';
 require('./favicon.ico');
 import './styles/styles.scss';
-
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
-
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Flexbox from 'flexbox-react';
-
-import { twitterCard, AsyncTwitter } from './components/infocards/twitter-card';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
@@ -27,43 +22,8 @@ class ProfileApp extends React.Component {
     }
 
     componentDidMount() {
-        const compile = extendedMarkdown => { console.error("" + this.props.profileId + "Not implemented - talk to Max"); return {
-            right: {
-                name: "Chris del Chris",
-                currentPosition: "Janitor",
-                currentCompany: "Macy's",
-                pictureUrl: "https://media.licdn.com/media/AAEAAQAAAAAAAAmEAAAAJDNjMDVmNzdkLTlmYWYtNGU1OC1hNjUwLWQ2MmQ4OWY1ZTY2OQ.jpg",
-                twitter: "potus", 
-                cards: [{
-                    title: "Card Title 1",
-                    content: "* bullet 11\n * bullet 2\n * bullet 3"
-                },
-                {
-                    title: "Card Title 2",
-                    content: "* bullet 12\n * bullet 2\n * bullet 3"
-                },
-                {
-                    title: "Card Title 3",
-                    content: "* bullet 13\n * bullet 2\n * bullet 3"
-                },
-                {
-                    title: "Card Title 4",
-                    content: "* bullet 14\n * bullet 2\n * bullet 3"
-                },
-                {
-                    title: "Card Title 5",
-                    content: "* bullet 15\n * bullet 2\n * bullet 3"
-                },
-                {
-                    title: "Card Title 6",
-                    content: "* bullet 16\n * bullet 2\n * bullet 3"
-                }]
-            }
-        }}
-
-        Promise.resolve({asdf:"asdf"})
-        .then(json => { console.error('Talk to Max to get the url'); return json; })
-        .then(compile)
+        fetch("/profile/" + this.props.profileId)
+        .then(r => r.json())
         .then(profile => {
             this.setState({profile: profile})
         })
@@ -142,7 +102,7 @@ const AppWrapper = ({children}) =>
 const App = ({}) =>
     <Router>
         <AppWrapper>
-            <Route path="/app/:profileId" component={ProfileAppRouteAdapter} />
+            <Route path="/app/profile/:profileId" component={ProfileAppRouteAdapter} />
         </AppWrapper>
     </Router>
 
